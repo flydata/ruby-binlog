f448f3ce6b84eac51c73741acd3cb58c6d60e94e
Zivago Lee <z@flydata.com>
2015-01-21 15:55:02 -0800

    add opt paths

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,19 @@
 require 'mkmf'
 
+HEADER_DIRS = [
+  '/opt/flydata/include',
+  '/usr/local/include',
+  '/usr/include',
+]
+
+LIB_DIRS = [
+  '/opt/flydata/lib',
+  '/usr/local/lib',
+  '/usr/lib',
+]
+
+dir_config('replication', HEADER_DIRS, LIB_DIRS)
+
 if have_library('stdc++') and have_library('replication')
   create_makefile('binlog')
 end
