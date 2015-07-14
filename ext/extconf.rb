23fa0da4b81447baff42bc3acedf644aec9dd8d1
Zivago Lee <z@flydata.com>
2015-07-14 14:01:34 -0700

    add options for mysql replication listener library detection
    When using these options, you can provide custom directories for the
    replication library and headers:
    
    --with-replication-lib=
    --with-replication-include=
    
    ex.
    $ ruby extconf.rb
    -with-replication-lib=~/mysql-replication-listener-master/lib
    --with-replication-include=~/mysql-replication-listener-master/include

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,24 @@
 require 'mkmf'
 
+LIBDIR      = RbConfig::CONFIG['libdir']
+INCLUDEDIR  = RbConfig::CONFIG['includedir']
+
+HEADER_DIRS = [
+  '/opt/local/include',
+  '/usr/local/include',
+  INCLUDEDIR,
+  '/usr/include',
+]
+
+LIB_DIRS = [
+  '/opt/local/lib',
+  '/usr/local/lib',
+  LIBDIR,
+  '/usr/lib',
+]
+
+dir_config('replication', HEADER_DIRS, LIB_DIRS)
+
 if have_library('stdc++') and have_library('replication')
   create_makefile('binlog')
 end
