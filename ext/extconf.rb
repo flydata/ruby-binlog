48bc78b82a595718bc179d95b434912578914fda
Zivago Lee <z@flydata.com>
2015-07-14 17:19:20 -0700

    add --with-replication-rpath option
    If using non-standard path to the mysql-replication-listener libraries
    (or not in LD cache), we need to explicitly set rpath as well.
    
    ex.
    --with-replication-rpath=

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -19,6 +19,18 @@ LIB_DIRS = [
 
 dir_config('replication', HEADER_DIRS, LIB_DIRS)
 
+case explicit_rpath = with_config('replication-rpath')
+when true
+  abort "-----\nOption --with-replication-rpath must have an argument\n-----"
+when false
+  warn "-----\nOption --with-replication-rpath has been disabled at your request\n-----"
+when String
+  # The user gave us a value so use it
+  rpath_flags = " -Wl,-rpath,#{explicit_rpath}"
+  warn "-----\nSetting replication rpath to #{explicit_rpath}\n-----"
+  $LDFLAGS << rpath_flags
+end
+
 if have_library('stdc++') and have_library('replication')
   create_makefile('binlog')
 end
