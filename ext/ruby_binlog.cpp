6913a580df54c969c76233debbfb17a8c6fe9185
Chris Williams <chris.williams@bookrenter.com>
2015-10-19 17:21:02 -0700

    fix slow growing memory leak
    I suspect this is due to rb_raise jumping out of the catch and the c++ exception never being freed

--- ext/ruby_binlog.cpp
+++ ext/ruby_binlog.cpp
@@ -63,6 +63,7 @@ struct Client {
 
   static VALUE connect(VALUE self) {
     Client *p;
+    VALUE rberr=NULL;
     int result;
 
     Data_Get_Struct(self, Client, p);
@@ -72,8 +73,11 @@ struct Client {
     try {
       result = p->m_binlog->connect();
     } catch (const std::exception& e) {
-      rb_raise(rb_eBinlogError, "%s", e.what());
+      rberr = rb_exc_new2(rb_eBinlogError, e.what());
     }
+    
+    if (rberr)
+      rb_exc_raise(rberr);
 #ifndef RUBY_UBF_IO
     TRAP_END;
 #endif
