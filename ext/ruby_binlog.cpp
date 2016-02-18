08bc632dd8bf237ce6e91991141bfde10f94e00c
Mak Inada <github@inada.com>
2016-02-18 20:38:07 +0000

    @T4337:R added #disconnect method

--- ext/ruby_binlog.cpp
+++ ext/ruby_binlog.cpp
@@ -85,22 +85,17 @@ struct Client {
     return (result == 0) ? Qtrue : Qfalse;
   }
 
-  // XXX: Don't use
-  /*
   static VALUE disconnect(VALUE self) {
     Client *p;
-    mysql::system::Binlog_tcp_driver *driver;
 
     Data_Get_Struct(self, Client, p);
-    driver = cast_to_tcp_driver(p->m_binlog->m_driver);
 
-    if (driver) {
-      driver->disconnect();
+    if (p->m_binlog) {
+      p->m_binlog->disconnect();
     }
 
     return Qnil;
   }
-   */
 
   // XXX: Don't use
   /*
@@ -430,8 +425,8 @@ struct Client {
     rb_define_alloc_func(rb_cBinlogClient, &alloc);
     rb_define_private_method(rb_cBinlogClient, "initialize", __F(&initialize), 1);
     rb_define_method(rb_cBinlogClient, "connect", __F(&connect), 0);
+    rb_define_method(rb_cBinlogClient, "disconnect", __F(&disconnect), 0);
     // XXX: Don't use
-    //rb_define_method(rb_cBinlogClient, "disconnect", __F(&disconnect), 0);
     //rb_define_method(rb_cBinlogClient, "reconnect", __F(&reconnect), 0);
     rb_define_method(rb_cBinlogClient, "closed?", __F(&is_closed), 0);
     rb_define_method(rb_cBinlogClient, "wait_for_next_event", __F(&wait_for_next_event), 0);
